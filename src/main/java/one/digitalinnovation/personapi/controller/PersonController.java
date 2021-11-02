730d743eb52c24d0a6b2a89b2edcd6a3cfc57589
aldo-falcao <clodoaldo.falcao@gmail.com>
2021-11-01 23:35:15 -0400

    Desenvolvido metodo delete e update

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -17,26 +17,37 @@ import one.digitalinnovation.personapi.service.PersonService;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-	private PersonService personService;
+    private PersonService personService;
 
-	@Autowired
-	public PersonController(PersonService personService) {
-		this.personService = personService;
-	}
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
-		return personService.createPerson(personDTO);
-	}
-	
-	@GetMapping
-	public List<PersonDTO> listAll() {
-		return personService.listAll();
-	}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
 
-	@GetMapping("/{id}")
-	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
-		return personService.findById(id);
-	}
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
 }
